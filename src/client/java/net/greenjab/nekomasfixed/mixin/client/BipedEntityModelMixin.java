81ebd42d92adb2a29e4d860eae9cc75d61beb0f1
joshua <joshua@baikie.org>
2026-01-24 11:36:56 +1100

    proper model and textures for the slingshot

--- src/client/java/net/greenjab/nekomasfixed/mixin/client/BipedEntityModelMixin.java
+++ src/client/java/net/greenjab/nekomasfixed/mixin/client/BipedEntityModelMixin.java
@@ -1,5 +1,6 @@
 package net.greenjab.nekomasfixed.mixin.client;
 
+import net.greenjab.nekomasfixed.registry.registries.ItemRegistry;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.state.BipedEntityRenderState;
@@ -19,7 +20,7 @@ public class BipedEntityModelMixin <T extends BipedEntityRenderState> {
     private void rightArmSlingshot(T state, CallbackInfo ci) {
         if (state.rightArmPose == BOW_AND_ARROW) {
             BipedEntityModel<?> BEM = (BipedEntityModel<?>) (Object) this;
-            charge(BEM.rightArm, BEM.leftArm, BEM.head, state.crossbowPullTime, state.itemUseTime, true);
+            charge(BEM.rightArm, BEM.leftArm, BEM.head, state.crossbowPullTime, state.itemUseTime, true, state.rightHandItem.isOf(ItemRegistry.SLINGSHOT));
             ci.cancel();
         }
     }
@@ -28,20 +29,21 @@ public class BipedEntityModelMixin <T extends BipedEntityRenderState> {
     private void leftArmSlingshot(T state, CallbackInfo ci) {
         if (state.leftArmPose == BOW_AND_ARROW) {
             BipedEntityModel<?> BEM = (BipedEntityModel<?>) (Object) this;
-            charge(BEM.rightArm, BEM.leftArm, BEM.head, state.crossbowPullTime, state.itemUseTime, false);
+            //if state.activeHand == Hand.MAIN_HAND
+            charge(BEM.rightArm, BEM.leftArm, BEM.head, state.crossbowPullTime, state.itemUseTime, false, state.leftHandItem.isOf(ItemRegistry.SLINGSHOT));
             ci.cancel();
         }
     }
 
     @Unique
-    private static void charge(ModelPart holdingArm, ModelPart pullingArm,ModelPart head, float crossbowPullTime, float f, boolean rightArm) {
+    private static void charge(ModelPart holdingArm, ModelPart pullingArm,ModelPart head, float crossbowPullTime, float f, boolean rightArm, boolean slingshot) {
         ModelPart modelPart = rightArm ? holdingArm : pullingArm;
         ModelPart modelPart2 = rightArm ? pullingArm : holdingArm;
 
         modelPart.pitch = MathHelper.clamp(head.pitch, -1.2F, 1.2F) - 1.4835298F;
         modelPart.yaw = 0.7f*head.yaw - (float) (Math.PI / 12) * (rightArm ? 1 : -1);
 
-        modelPart2.pitch = modelPart.pitch;
+        modelPart2.pitch = modelPart.pitch+(slingshot?-0.7f:0);
         float g = MathHelper.clamp(f, 0.0F, crossbowPullTime);
         float h = g / crossbowPullTime;
         modelPart2.yaw = (float) (-Math.sin(modelPart2.pitch)*MathHelper.lerp(h, 0.4F, 0.85F) * (rightArm ? 1 : -1));
