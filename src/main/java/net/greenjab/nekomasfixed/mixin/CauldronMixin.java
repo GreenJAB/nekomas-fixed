dd29ccf2889356377b507457c8c3cca5f7b44f44
cybersage005-netizen <cybersage005@gmail.com>
2026-02-23 23:12:35 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/net/greenjab/nekomasfixed/mixin/CauldronMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/CauldronMixin.java
@@ -35,20 +35,22 @@ public class CauldronMixin {
     private void onCauldronUse(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         System.out.println(stack);
 
-        if (!world.isClient()) {
+
+        if (stack.isOf(Items.WATER_BUCKET) && state.getBlock() == Blocks.CAULDRON) {
             TagKey<Biome> COLD_BIOMES = TagKey.of(RegistryKeys.BIOME,
                     Identifier.of("c", "climate/cold"));
             boolean isCold = world.getBiome(pos).isIn(COLD_BIOMES);
 
-            if (stack.isOf(Items.WATER_BUCKET) && isCold) {
+            if (isCold && !world.isClient()) {
                 world.setBlockState(pos, BlockRegistry.ICE_CAULDRON.getDefaultState()
                         .with(IceCauldronBlock.ICE_LEVEL, 1));
-                world.playSound(null, pos, SoundEvents.BLOCK_LAVA_EXTINGUISH,
+                // Replace water bucket with empty bucket
+                player.setStackInHand(hand, new ItemStack(Items.BUCKET));
+                world.playSound(null, pos, SoundEvents.BLOCK_GLASS_PLACE,
                         SoundCategory.BLOCKS, 1.0F, 1.0F);
-                System.out.println("WORKING");
-
-            } else {
-
+                System.out.println("Water froze into ice cauldron!");
+                cir.setReturnValue(ActionResult.SUCCESS);
+                return;
             }
         }
 
