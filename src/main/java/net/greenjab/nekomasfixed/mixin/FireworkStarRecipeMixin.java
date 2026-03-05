7ea5dfad7325d09ee6f2c8b4263d75aeb7e6327c
cybersage005-netizen <cybersage005@gmail.com>
2026-03-05 20:49:52 +0530

    dyes-dont-check-this-one

--- src/main/java/net/greenjab/nekomasfixed/mixin/FireworkStarRecipeMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/FireworkStarRecipeMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.item.Items;
 import net.minecraft.recipe.FireworkStarRecipe;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.input.CraftingRecipeInput;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -22,93 +23,89 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
-//Note for GREEN_JAB: Plz check this class once, i am not sure if it going to override vanilla logic and give support to the trials and effects for fireworks
 @Mixin(FireworkStarRecipe.class)
 public class FireworkStarRecipeMixin {
     @Unique
     private boolean isColorFromMod(Item item){
-        if (
-                item.equals(ItemRegistry.AMBER_DYE)
-                ||
-                item.equals(ItemRegistry.CRIMSON_DYE)
-                ||
-                item.equals(ItemRegistry.INDIGO_DYE)
-                ||
-                item.equals(ItemRegistry.AQUA_DYE)
-        ) {
-           return true;
-        }return false;
+        return item.equals(ItemRegistry.AMBER_DYE) ||
+                item.equals(ItemRegistry.CRIMSON_DYE) ||
+                item.equals(ItemRegistry.INDIGO_DYE) ||
+                item.equals(ItemRegistry.AQUA_DYE);
     }
 
     @Inject(method = "matches(Lnet/minecraft/recipe/input/CraftingRecipeInput;Lnet/minecraft/world/World;)Z", at = @At("HEAD"), cancellable = true)
     private void matches(CraftingRecipeInput craftingRecipeInput, World world, CallbackInfoReturnable<Boolean> cir) {
         if (craftingRecipeInput.getStackCount() < 2) {
             cir.setReturnValue(false);
-        } else {
-            boolean bl = false;
-            boolean bl2 = false;
-            boolean bl3 = false;
-            boolean bl4 = false;
-            boolean bl5 = false;
+            return;
+        }
 
-            for(int i = 0; i < craftingRecipeInput.size(); ++i) {
-                ItemStack itemStack = craftingRecipeInput.getStackInSlot(i);
-                final Map<Item, FireworkExplosionComponent.Type> TYPE_MODIFIER_MAP;
-                final Ingredient TRAIL_MODIFIER;
-                final Ingredient FLICKER_MODIFIER;
+        boolean bl = false;
+        boolean bl2 = false;
+        boolean bl3 = false;
+        boolean bl4 = false;
+        boolean bl5 = false;
 
-                TYPE_MODIFIER_MAP = Map.of(Items.FIRE_CHARGE, FireworkExplosionComponent.Type.LARGE_BALL, Items.FEATHER, FireworkExplosionComponent.Type.BURST, Items.GOLD_NUGGET, FireworkExplosionComponent.Type.STAR, Items.SKELETON_SKULL, FireworkExplosionComponent.Type.CREEPER, Items.WITHER_SKELETON_SKULL, FireworkExplosionComponent.Type.CREEPER, Items.CREEPER_HEAD, FireworkExplosionComponent.Type.CREEPER, Items.PLAYER_HEAD, FireworkExplosionComponent.Type.CREEPER, Items.DRAGON_HEAD, FireworkExplosionComponent.Type.CREEPER, Items.ZOMBIE_HEAD, FireworkExplosionComponent.Type.CREEPER, Items.PIGLIN_HEAD, FireworkExplosionComponent.Type.CREEPER);
-                TRAIL_MODIFIER = Ingredient.ofItem(Items.DIAMOND);
-                FLICKER_MODIFIER = Ingredient.ofItem(Items.GLOWSTONE_DUST);
+        final Map<Item, FireworkExplosionComponent.Type> TYPE_MODIFIER_MAP = Map.of(
+                Items.FIRE_CHARGE, FireworkExplosionComponent.Type.LARGE_BALL,
+                Items.FEATHER, FireworkExplosionComponent.Type.BURST,
+                Items.GOLD_NUGGET, FireworkExplosionComponent.Type.STAR,
+                Items.SKELETON_SKULL, FireworkExplosionComponent.Type.CREEPER,
+                Items.WITHER_SKELETON_SKULL, FireworkExplosionComponent.Type.CREEPER,
+                Items.CREEPER_HEAD, FireworkExplosionComponent.Type.CREEPER,
+                Items.PLAYER_HEAD, FireworkExplosionComponent.Type.CREEPER,
+                Items.DRAGON_HEAD, FireworkExplosionComponent.Type.CREEPER,
+                Items.ZOMBIE_HEAD, FireworkExplosionComponent.Type.CREEPER,
+                Items.PIGLIN_HEAD, FireworkExplosionComponent.Type.CREEPER
+        );
 
-                final Ingredient GUNPOWDER = Ingredient.ofItem(Items.GUNPOWDER);
-                if (!itemStack.isEmpty()) {
+        final Ingredient TRAIL_MODIFIER = Ingredient.ofItems(Items.DIAMOND);
+        final Ingredient FLICKER_MODIFIER = Ingredient.ofItems(Items.GLOWSTONE_DUST);
+        final Ingredient GUNPOWDER = Ingredient.ofItems(Items.GUNPOWDER);
 
-
-                    if (TYPE_MODIFIER_MAP.containsKey(itemStack.getItem())) {
-                        if (bl3) {
-                            cir.setReturnValue(false);
-                        }
-
-                        bl3 = true;
-                    } else if (FLICKER_MODIFIER.test(itemStack)) {
-                        if (bl5) {
-                            cir.setReturnValue(false);
-
-                        }
-
-                        bl5 = true;
-                    } else if (TRAIL_MODIFIER.test(itemStack)) {
-                        if (bl4) {
-                            cir.setReturnValue(false);
-
-                        }
-
-                        bl4 = true;
-                    } else if (GUNPOWDER.test(itemStack)) {
-                        if (bl) {
-                            cir.setReturnValue(false);
-
-                        }
-
-                        bl = true;
-                    } else {
-                        if (!(itemStack.getItem() instanceof DyeItem && isColorFromMod(itemStack.getItem()))) {
-                            cir.setReturnValue(false);
-                        }
-
-                        bl2 = true;
+        for(int i = 0; i < craftingRecipeInput.size(); ++i) {
+            ItemStack itemStack = craftingRecipeInput.getStackInSlot(i);
+            if (!itemStack.isEmpty()) {
+                if (TYPE_MODIFIER_MAP.containsKey(itemStack.getItem())) {
+                    if (bl3) {
+                        cir.setReturnValue(false);
+                        return;
                     }
+                    bl3 = true;
+                } else if (FLICKER_MODIFIER.test(itemStack)) {
+                    if (bl5) {
+                        cir.setReturnValue(false);
+                        return;
+                    }
+                    bl5 = true;
+                } else if (TRAIL_MODIFIER.test(itemStack)) {
+                    if (bl4) {
+                        cir.setReturnValue(false);
+                        return;
+                    }
+                    bl4 = true;
+                } else if (GUNPOWDER.test(itemStack)) {
+                    if (bl) {
+                        cir.setReturnValue(false);
+                        return;
+                    }
+                    bl = true;
+                } else {
+                    if (!(itemStack.getItem() instanceof DyeItem) && !isColorFromMod(itemStack.getItem())) {
+                        cir.setReturnValue(false);
+                        return;
+                    }
+                    bl2 = true;
                 }
             }
-
-            cir.setReturnValue(bl&&bl2);
-
         }
+
+        cir.setReturnValue(bl && bl2);
     }
 
-    @Inject(method = "craft" , at = @At("TAIL"), cancellable = true)
-    private void craftCustom(CraftingRecipeInput craftingRecipeInput, World world, CallbackInfoReturnable<ItemStack> cir) {
+    @Inject(method = "craft(Lnet/minecraft/recipe/input/CraftingRecipeInput;Lnet/minecraft/registry/RegistryWrapper$WrapperLookup;)Lnet/minecraft/item/ItemStack;",
+            at = @At("RETURN"), cancellable = true)
+    private void craftCustom(CraftingRecipeInput craftingRecipeInput, RegistryWrapper.WrapperLookup lookup, CallbackInfoReturnable<ItemStack> cir) {
         FireworkExplosionComponent.Type type = FireworkExplosionComponent.Type.SMALL_BALL;
         boolean bl = false;
         boolean bl2 = false;
@@ -117,25 +114,22 @@ public class FireworkStarRecipeMixin {
         for(int i = 0; i < craftingRecipeInput.size(); ++i) {
             ItemStack itemStack = craftingRecipeInput.getStackInSlot(i);
             if (!itemStack.isEmpty()) {
-                    Item var11 = itemStack.getItem();
+                Item var11 = itemStack.getItem();
 
-                    if(isColorFromMod(var11)){
-                        System.out.println("WOOWWWW! How exciting it is!! Your craft method of fireworkstarrecipemixin works which no one care about, yeah!!!");
-                        ModColors modColors = ModColors.AMBER;
-                        if(var11.equals(ItemRegistry.AQUA_DYE)){
-                            modColors = ModColors.AQUA;
-                        }if(var11.equals(ItemRegistry.CRIMSON_DYE)){
-                            modColors = ModColors.CRIMSON;
-                        }if(var11.equals(ItemRegistry.INDIGO_DYE)){
-                            modColors = ModColors.INDIGO;
-                        }
-                        intList.add(modColors.getColor());
+                if (isColorFromMod(var11)) {
+                    System.out.println("Custom dye detected!");
+                    ModColors modColors = ModColors.AMBER;
+                    if (var11.equals(ItemRegistry.AQUA_DYE)) {
+                        modColors = ModColors.AQUA;
+                    } else if (var11.equals(ItemRegistry.CRIMSON_DYE)) {
+                        modColors = ModColors.CRIMSON;
+                    } else if (var11.equals(ItemRegistry.INDIGO_DYE)) {
+                        modColors = ModColors.INDIGO;
                     }
-                    else if(var11 instanceof DyeItem) {
-                        DyeItem dyeItem = (DyeItem)var11;
-                        intList.add(dyeItem.getColor().getFireworkColor());
-                    }
-
+                    intList.add(modColors.getColor());
+                } else if (var11 instanceof DyeItem dyeItem) {
+                    intList.add(dyeItem.getColor().getFireworkColor());
+                }
             }
         }
 
