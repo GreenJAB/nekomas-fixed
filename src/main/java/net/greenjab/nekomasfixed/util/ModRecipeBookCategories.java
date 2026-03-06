df23822e2a50e10450290cdd445b4ad2c810f230
cybersage005-netizen <cybersage005@gmail.com>
2026-03-06 21:25:04 +0530

    kiln-fix-(Check for errors and crashes - it could happen anytime)-MAIN

--- src/main/java/net/greenjab/nekomasfixed/util/ModRecipeBookCategories.java
+++ src/main/java/net/greenjab/nekomasfixed/util/ModRecipeBookCategories.java
@@ -5,7 +5,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class ModRecipeBookCategories {
-    public static final RecipeBookCategory KILNING_MISC = register("kilning");
+    public static final RecipeBookCategory KILNING_MISC = register("kilning_misc");
 
     private static RecipeBookCategory register(String id) {
         return (RecipeBookCategory) Registry.register(Registries.RECIPE_BOOK_CATEGORY, id, new RecipeBookCategory());
