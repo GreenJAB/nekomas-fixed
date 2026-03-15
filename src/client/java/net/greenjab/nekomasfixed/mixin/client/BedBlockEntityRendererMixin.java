20e1b0bb50a94f4156d4fd12f5ab76e58c8d8bed
cybersage005-netizen <cybersage005@gmail.com>
2026-03-15 10:07:36 +0530

    bug-fixes-dont-check

--- src/client/java/net/greenjab/nekomasfixed/mixin/client/BedBlockEntityRendererMixin.java
+++ src/client/java/net/greenjab/nekomasfixed/mixin/client/BedBlockEntityRendererMixin.java
@@ -7,12 +7,16 @@ import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.block.entity.BedBlockEntityRenderer;
 import net.minecraft.client.render.block.entity.state.BedBlockEntityRenderState;
 import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BedBlockEntityRenderer.class)
@@ -36,5 +40,38 @@ public class BedBlockEntityRendererMixin {
                     )
             );
         }
+    }
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void useCustomTexture(
+            BedBlockEntityRenderState state,
+            MatrixStack matrices,
+            OrderedRenderCommandQueue queue,
+            CameraRenderState camera,
+            CallbackInfo ci) {
+
+        SpriteIdentifier custom =
+                ((CustomBedTextureHolder) state).nekomasfixed$getCustomTexture();
+
+        if (custom != null) {
+            System.out.println("Rendering amber bed with custom texture");
+        }
+    }
+
+    @ModifyVariable(
+            method = "render",
+            at = @At(value = "STORE"),
+            ordinal = 0
+    )
+    private SpriteIdentifier replaceTexture(SpriteIdentifier original, BedBlockEntityRenderState state) {
+
+        SpriteIdentifier custom =
+                ((CustomBedTextureHolder) state).nekomasfixed$getCustomTexture();
+
+        if (custom != null) {
+            return custom;
+        }
+
+        return original;
     }
 }
