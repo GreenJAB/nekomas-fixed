18969bcfeb3b6421ea02554a934f631e608d1407
cybersage005-netizen <cybersage005@gmail.com>
2026-03-18 20:40:53 +0530

    finale-march-dont-check-it

--- src/main/java/net/greenjab/nekomasfixed/mixin/ProjectileEntityMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/ProjectileEntityMixin.java
@@ -3,7 +3,11 @@ package net.greenjab.nekomasfixed.mixin;
 import it.unimi.dsi.fastutil.doubles.DoubleDoubleImmutablePair;
 import net.minecraft.entity.*;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
@@ -31,6 +35,19 @@ Vec3d pos = hitResult.getPos();
         if(lightningEntity!=null){
             lightningEntity.refreshPositionAfterTeleport(pos);
             world.spawnEntity(lightningEntity);
+            if(projectileEntity.getOwner() instanceof PlayerEntity player){
+                if(player.getBlockPos().isWithinDistance(projectileEntity.getVelocityAffectingPos(), 1.0d)){
+                    if(player.getEquippedStack(EquipmentSlot.HEAD).isOf(Items.COPPER_HELMET)&&
+                            player.getEquippedStack(EquipmentSlot.CHEST).isOf(Items.COPPER_CHESTPLATE)&&
+                            player.getEquippedStack(EquipmentSlot.LEGS).isOf(Items.COPPER_LEGGINGS)&&
+                            player.getEquippedStack(EquipmentSlot.FEET).isOf(Items.COPPER_BOOTS)){
+                        StatusEffectInstance st = new StatusEffectInstance(StatusEffects.SPEED, 10*20, 4, false, false, false);
+                        StatusEffectInstance stH = new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1*20, 2, false, false, false);
+                        player.addStatusEffect(st);
+                        player.addStatusEffect(stH);
+                    }
+                }
+            }
         }
     }
 }
