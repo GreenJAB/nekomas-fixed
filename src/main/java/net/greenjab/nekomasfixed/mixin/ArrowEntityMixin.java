f35b2e57fbbf1ccc1355797c6a1ad8ba4c9ce8d2
cybersage005-netizen <cybersage005@gmail.com>
2026-03-19 13:37:43 +0530

    finale-march-dont-check-it

--- src/main/java/net/greenjab/nekomasfixed/mixin/ArrowEntityMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/ArrowEntityMixin.java
@@ -1,10 +1,12 @@
 package net.greenjab.nekomasfixed.mixin;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,13 +17,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ArrowEntityMixin {
     @Inject(method = "onHit", at = @At("HEAD"), cancellable = true)
     private void onHit(LivingEntity target, CallbackInfo ci) {
-        BlockPos pos = target.getBlockPos();
-        AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(target.getEntityWorld(), target.getX(), target.getY(), target.getZ());
-        areaEffectCloudEntity.setRadius(3.0F);
-        areaEffectCloudEntity.setRadiusOnUse(-0.5F);
-        areaEffectCloudEntity.setDuration(600);
-        areaEffectCloudEntity.setWaitTime(10);
-        areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float)areaEffectCloudEntity.getDuration());
-        target.getEntityWorld().spawnEntity(areaEffectCloudEntity);
+        ArrowEntity arrowEntity = (ArrowEntity)(Object)this;
+        ItemStack arrow = arrowEntity.getItemStack();
+        PotionContentsComponent contents = arrow.getOrDefault(DataComponentTypes.POTION_CONTENTS, PotionContentsComponent.DEFAULT);
+
+        if(arrow!=null && contents!=null && contents != PotionContentsComponent.DEFAULT){
+            AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(target.getEntityWorld(), target.getX(), target.getY(), target.getZ());
+            areaEffectCloudEntity.setRadius(3.0F);
+            areaEffectCloudEntity.setRadiusOnUse(-0.5F);
+            areaEffectCloudEntity.setDuration(600);
+            areaEffectCloudEntity.setWaitTime(10);
+            areaEffectCloudEntity.setPotionContents(contents);
+            areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float)areaEffectCloudEntity.getDuration());
+            target.getEntityWorld().spawnEntity(areaEffectCloudEntity);
+        }
+
     }
 }
