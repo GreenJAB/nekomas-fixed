1a4f0153827f55d6a0ebceba1fb07470f911799f
cybersage005-netizen <cybersage005@gmail.com>
2026-03-21 18:23:25 +0530

    finale-march-dont-check-it

---
+++ src/main/java/net/greenjab/nekomasfixed/registry/registries/EnchantmentEffectRegistry.java
@@ -0,0 +1,20 @@
+package net.greenjab.nekomasfixed.registry.registries;
+
+import net.greenjab.nekomasfixed.NekomasFixed;
+import net.greenjab.nekomasfixed.enchantment.effect.DismountEnchantmentEffect;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class EnchantmentEffectRegistry {
+
+    public static void register() {
+        Registry.register(
+                Registries.ENCHANTMENT_ENTITY_EFFECT_TYPE,
+                Identifier.of("nekomasfixed", "leeching"),
+                DismountEnchantmentEffect.CODEC
+        );
+
+        NekomasFixed.LOGGER.info("Registered enchantment effects");
+    }
+}
