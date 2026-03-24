ad486882b59f6699f8c14cc5e995a286d800c35f
cybersage005-netizen <cybersage005@gmail.com>
2026-03-24 16:33:43 +0530

    last-prep-dont-check-it

--- src/main/java/net/greenjab/nekomasfixed/mixin/DolphinEntityMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/DolphinEntityMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,10 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DolphinEntity.class)
 public class DolphinEntityMixin {
-
-
-    @Unique
-    DolphinEntity dolphinEntity = (DolphinEntity) (Object) this;
 
     @Inject(method = "initDataTracker", at = @At("TAIL"))
     private void initCustomDataTracker(DataTracker.Builder builder, CallbackInfo ci) {
@@ -32,13 +27,15 @@ public class DolphinEntityMixin {
     private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         ItemStack stack = player.getStackInHand(Hand.MAIN_HAND);
         if(stack.isOf(Items.TROPICAL_FISH)){
-            dolphinEntity.getDataTracker().set(IsTropicalFishFedDataTracker.IS_TROPICAL_FISH_FED, true);
+            DolphinEntity dolphin = (DolphinEntity)(Object)this;
+            dolphin.getDataTracker().set(IsTropicalFishFedDataTracker.IS_TROPICAL_FISH_FED, true);
             stack.decrementUnlessCreative(1, player);
         }
     }
 
     @Inject(method = "initGoals", at = @At("TAIL"))
     private void initCustomGoals(CallbackInfo ci){
-        dolphinEntity.goalSelector.add(10, new MoveIntoWaterGoal(dolphinEntity));
+        DolphinEntity dolphin = (DolphinEntity)(Object)this;
+        dolphin.goalSelector.add(10, new MoveIntoWaterGoal(dolphin));
     }
 }
