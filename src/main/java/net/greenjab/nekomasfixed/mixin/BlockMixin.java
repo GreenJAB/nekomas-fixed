e8f55724e6ac81ac5b00d5603c27229c3454c0cb
cybersage005-netizen <cybersage005@gmail.com>
2026-03-24 20:46:26 +0530

    april-one=dont-check-it

--- src/main/java/net/greenjab/nekomasfixed/mixin/BlockMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/BlockMixin.java
@@ -24,7 +24,7 @@ public class BlockMixin {
             ItemStack stack = player.getMainHandStack();
             RegistryEntry<Enchantment> silkTouchEntry =
                     world.getRegistryManager().getEntryOrThrow(RegistryEntry.of(Enchantments.SILK_TOUCH).value());
-        System.out.println(silkTouchEntry);
+        System.out.println(silkTouchEntry + " is a noob");
             if(!stack.hasEnchantments() && stack.getEnchantments().getLevel(silkTouchEntry)>0){
                 System.out.println("LAVA LAVA LAVA");
                 player.getEntityWorld().setBlockState(pos, Blocks.LAVA.getDefaultState());
