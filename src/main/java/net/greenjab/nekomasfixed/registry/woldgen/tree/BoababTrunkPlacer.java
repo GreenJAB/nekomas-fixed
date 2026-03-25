90838d99162cef13dc05a48fd864cdc8956e8424
cybersage005-netizen <cybersage005@gmail.com>
2026-03-25 22:21:34 +0530

    april-one=dont-check-it-boabab-tree-trunk

--- src/main/java/net/greenjab/nekomasfixed/registry/woldgen/tree/BoababTrunkPlacer.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/woldgen/tree/BoababTrunkPlacer.java
@@ -63,7 +63,7 @@ public class BoababTrunkPlacer extends TrunkPlacer {
         for (int y = currentY; y < lowerPart; ++y) {
             for (int x = -girthRadius; x <= girthRadius; ++x) {
                 for (int z = -girthRadius; z <= girthRadius; ++z) {
-                        if(z<girthRadius || x<girthRadius){continue;}
+                        if(z<girthRadius && x<girthRadius){continue;}
 
                             BlockPos pos = startPos.add(x, y, z);
                             if (TreeFeature.isAirOrLeaves(world, pos)) {
