146284b10f4c63741e39c0d97c3eb4a3565ff29a
cybersage005-netizen <cybersage005@gmail.com>
2026-03-26 23:15:11 +0530

    april-one=dont-check-it-rope

--- src/main/java/net/greenjab/nekomasfixed/registry/block/RopeBlock.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/RopeBlock.java
@@ -31,9 +31,7 @@ public class RopeBlock extends  Block  {
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         BlockState blockState = ctx.getWorld().getBlockState(ctx.getBlockPos().up());
         BlockState blockState2 = ctx.getWorld().getBlockState(ctx.getBlockPos());
-        if(!blockState.isAir()){
-            return blockState2.with(IS_CONNECTED, false);
-        }else if(!blockState.isAir() && !blockState2.get(IS_CONNECTED, false)){
+         if(!blockState.isAir() && !blockState2.get(IS_CONNECTED, false)){
             return blockState2.with(IS_CONNECTED, true);
         }
         else{
