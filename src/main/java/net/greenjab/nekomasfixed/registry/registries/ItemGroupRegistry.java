46a1357d994544899b69996efe05619ba3c3d335
cybersage005-netizen <cybersage005@gmail.com>
2026-03-28 22:03:45 +0530

    MAY-release=dci

--- src/main/java/net/greenjab/nekomasfixed/registry/registries/ItemGroupRegistry.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/registries/ItemGroupRegistry.java
@@ -127,10 +127,7 @@ public class ItemGroupRegistry {
                          entries.add(ItemRegistry.CRIMSON_HARNESS);
 
                          //new things
-                         entries.add(ItemRegistry.BOABAB_SAPLING);
-                         entries.add(ItemRegistry.BOABAB_LOG);
                          entries.add(ItemRegistry.BOABAB_FRUIT);
-                         entries.add(ItemRegistry.ROPE_ITEM);
 
                          entries.add(ItemRegistry.WHITE_FROGLIGHT);
                          entries.add(ItemRegistry.LIGHT_GRAY_FROGLIGHT);
