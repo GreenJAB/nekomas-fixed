59619ab232f3119f9818b6b5d37e99d2a6a79f73
cybersage005-netizen <cybersage005@gmail.com>
2026-03-28 22:27:38 +0530

    MAY-release=dci

---
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/BoababFruitBlock.java
@@ -0,0 +1,63 @@
+package net.greenjab.nekomasfixed.registry.block;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
+import org.jspecify.annotations.Nullable;
+
+public class BoababFruitBlock extends Block implements Fertilizable {
+    public static final MapCodec<BoababFruitBlock> CODEC = createCodec(BoababFruitBlock::new);
+    public static final int MAX_AGE = 2;
+    public static final IntProperty AGE = IntProperty.of("age", 0, 1);
+
+    public BoababFruitBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(AGE, 0));
+    }
+
+    @Override
+    protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return !world.getBlockState(pos.up()).isAir() || !world.getBlockState(pos.down()).isAir();
+    }
+
+    @Override
+    public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState blockState = this.getDefaultState();
+        WorldView worldView = ctx.getWorld();
+        BlockPos blockPos = ctx.getBlockPos();
+        if(canPlaceAt(blockState, worldView, blockPos)){
+            return this.getDefaultState();
+        }
+        return null;
+    }
+
+    @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+        return !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+    }
+
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        return state.get(AGE) < 2;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        world.setBlockState(pos, state.with(AGE, state.get(AGE) + 1), 2);
+    }
+}
