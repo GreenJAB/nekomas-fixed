d703dbecd4ac7a578cd7dc1279216cec853b63dc
cybersage005-netizen <cybersage005@gmail.com>
2026-03-30 21:55:40 +0530

    MAY-release=dci-termites

--- src/main/java/net/greenjab/nekomasfixed/registry/entity/Termite/TermiteEntity.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/entity/Termite/TermiteEntity.java
@@ -1,6 +1,7 @@
 package net.greenjab.nekomasfixed.registry.entity.Termite;
 
 import net.minecraft.entity.AnimationState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -9,6 +10,8 @@ import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -17,6 +20,7 @@ import java.util.Optional;
 public class TermiteEntity extends HostileEntity {
     public final AnimationState idleAnimationState = new AnimationState();
     public final AnimationState runAnimationState = new AnimationState();
+    public final AnimationState swipeAnimationState = new AnimationState();
     int idleAnimationTimeout = 0;
 
     public TermiteEntity(EntityType<? extends HostileEntity> entityType, World world) {
@@ -44,6 +48,21 @@ public class TermiteEntity extends HostileEntity {
                 .add(EntityAttributes.MOVEMENT_SPEED, 0.4d)
                 .add(EntityAttributes.SAFE_FALL_DISTANCE, 2d)
                 .add(EntityAttributes.STEP_HEIGHT, 1d);
+    }
+
+    public boolean tryAttack(ServerWorld world, Entity target) {
+        world.sendEntityStatus(this, (byte)10);
+        this.playSound(SoundEvents.ENTITY_SILVERFISH_AMBIENT, 10.0F, this.getSoundPitch());
+        return super.tryAttack(world, target);
+    }
+
+    @Override
+    public void handleStatus(byte status) {
+        if (status == 10) {
+            this.swipeAnimationState.start(this.age);
+        } else {
+            super.handleStatus(status);
+        }
     }
 
     private void setupAnimationStates() {
@@ -105,6 +124,14 @@ public class TermiteEntity extends HostileEntity {
         @Override
         public boolean shouldContinue() {
             return !termiteEntity.getNavigation().isIdle();
+        }
+
+        public BlockPos getTargetPos() {
+            return targetPos;
+        }
+
+        public void setTargetPos(BlockPos targetPos) {
+            this.targetPos = targetPos;
         }
     }
 
