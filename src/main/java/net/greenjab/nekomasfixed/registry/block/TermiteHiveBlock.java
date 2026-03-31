2712d75376489d02dd5cdb9b8f3e85e29391fc05
cybersage005-netizen <cybersage005@gmail.com>
2026-03-31 23:55:13 +0530

    MAY-release=dci-termites

--- src/main/java/net/greenjab/nekomasfixed/registry/block/TermiteHiveBlock.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/TermiteHiveBlock.java
@@ -6,6 +6,7 @@ import net.greenjab.nekomasfixed.registry.registries.BlockEntityTypeRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
@@ -20,6 +21,7 @@ import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.loot.context.LootWorldContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -35,15 +37,16 @@ import java.util.List;
 public class TermiteHiveBlock extends BlockWithEntity {
     public static final MapCodec<TermiteHiveBlock> CODEC = createCodec(TermiteHiveBlock::new);
     public static IntProperty TERMITES = IntProperty.of("termites", 0, 2);
+    public static final EnumProperty<Direction> FACING = EnumProperty.of("facing", Direction.class);
     public TermiteHiveBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.getDefaultState().with(TERMITES, 0));
+        this.setDefaultState(this.getDefaultState().with(TERMITES, 0).with(FACING, Direction.NORTH));
     }
 
     @Override
     protected MapCodec<? extends BlockWithEntity> getCodec() {return CODEC;}
 
-    private boolean hasBees(World world, BlockPos pos) {
+    private boolean hasTermites(World world, BlockPos pos) {
         BlockEntity blockEntity = world.getBlockEntity(pos);
         if (blockEntity instanceof TermiteHiveBlockEntity termiteHiveBlockEntity) {
             return !termiteHiveBlockEntity.hasNoTermites();
@@ -52,26 +55,27 @@ public class TermiteHiveBlock extends BlockWithEntity {
         }
     }
 
-    @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState();
-    }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(TERMITES);
+        builder.add(TERMITES, FACING);
     }
 
     @Override
     public <T extends BlockEntity> @Nullable BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        if (world.isClient()) {
-            return null;
-        }
+        if (world.isClient()) return null;
+
         if (type == BlockEntityTypeRegistry.TERMITE_HIVE_BLOCK_ENTITY) {
             return (world1, pos, state1, blockEntity) -> {
                 if (blockEntity instanceof TermiteHiveBlockEntity hive) {
                     TermiteHiveBlockEntity.serverTick(world1, pos, state1, hive);
-                    world1.setBlockState(pos, state1.with(TERMITES, hive.getTermiteCount()), 3);
+
+                    int current = state1.get(TERMITES);
+                    int actual = hive.getTermiteCount();
+
+                    if (current != actual) {
+                        world1.setBlockState(pos, state1.with(TERMITES, actual), 3);
+                    }
                 }
             };
         }
@@ -82,7 +86,7 @@ public class TermiteHiveBlock extends BlockWithEntity {
     @Override
     public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
         if (world instanceof ServerWorld serverWorld) {
-            if (player.shouldSkipBlockDrops() && serverWorld.getGameRules().getValue(GameRules.DO_TILE_DROPS)) {
+            if (player.shouldSkipBlockDrops() && !world.isClient() && serverWorld.getGameRules().getValue(GameRules.DO_TILE_DROPS)) {
                 BlockEntity blockEntity = world.getBlockEntity(pos);
                 if (blockEntity instanceof TermiteHiveBlockEntity termiteHiveBlockEntity) {
                     int i = state.get(TERMITES);
@@ -117,10 +121,6 @@ public class TermiteHiveBlock extends BlockWithEntity {
         return itemStack;
     }
 
-    @Override
-    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
-        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
-    }
 
     @Override
     public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
