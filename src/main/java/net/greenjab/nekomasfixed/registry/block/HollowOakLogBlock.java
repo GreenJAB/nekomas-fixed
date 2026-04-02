6d264612fdcd11db490d77fc636dfdb971fdbb8c
cybersage005-netizen <cybersage005@gmail.com>
2026-04-02 20:58:46 +0530

    APRIL=dci-hollow

--- src/main/java/net/greenjab/nekomasfixed/registry/block/HollowOakLogBlock.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/HollowOakLogBlock.java
@@ -7,18 +7,17 @@ import net.minecraft.block.ShapeContext;
 import net.minecraft.util.Util;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
 public class HollowOakLogBlock extends PillarBlock {
-    private static final VoxelShape RAYCAST_SHAPE = Block.createColumnShape((double)12.0F, (double)0.0F, (double)16.0F);
-    public static final VoxelShape OUTLINE_SHAPE = Util.make(() -> {
+    private static final VoxelShape RAYCAST_SHAPE = Block.createColumnShape((double)12.0F, (double)4.0F, (double)16.0F);
+    public static final VoxelShape OUTLINE_SHAPE = (VoxelShape) Util.make(() -> {
         int i = 4;
         int j = 3;
         int k = 2;
-        return VoxelShapes.cuboid(new Box(0f, 0f, 0f, 1f, 1f, 1f));
+        return VoxelShapes.combineAndSimplify(VoxelShapes.fullCube(), VoxelShapes.union(Block.createColumnShape((double)16.0F, (double)8.0F, (double)0.0F, (double)3.0F), new VoxelShape[]{Block.createColumnShape((double)8.0F, (double)16.0F, (double)0.0F, (double)3.0F), Block.createColumnShape((double)12.0F, (double)0.0F, (double)3.0F), RAYCAST_SHAPE}), BooleanBiFunction.ONLY_FIRST);
     });
     public HollowOakLogBlock(Settings settings) {
         super(settings);
