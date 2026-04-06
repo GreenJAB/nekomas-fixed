c0d93282bc6a0f320fb63e0a7b0256b2c69b1ec0
cybersage005-netizen <cybersage005@gmail.com>
2026-04-06 23:19:55 +0530

    APRIL=dci-hollow

--- src/main/java/net/greenjab/nekomasfixed/registry/block/HollowOakLogBlock.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/HollowOakLogBlock.java
@@ -13,20 +13,20 @@ import net.minecraft.world.BlockView;
 
 public class HollowOakLogBlock extends PillarBlock {
     private static final VoxelShape RAYCAST_SHAPE = Block.createColumnShape(12.0F, 4.0F, 16.0F);
-    public static final VoxelShape OUTLINE_SHAPE = Util.make(() -> VoxelShapes.combineAndSimplify(
-            VoxelShapes.fullCube(),
-            VoxelShapes.union(Block.createColumnShape(16.0F, 8.0F, 0.0F, 0.0F),
-                    Block.createColumnShape(8.0F, 16.0F, 0.0F, 0.0F),
-                    Block.createColumnShape(12.0F, 0.0F, 0.0F), RAYCAST_SHAPE),
-            BooleanBiFunction.ONLY_FIRST));
+//    public static final VoxelShape OUTLINE_SHAPE = Util.make(() -> VoxelShapes.combineAndSimplify(
+//            VoxelShapes.fullCube(),
+//            VoxelShapes.union(Block.createColumnShape(16.0F, 8.0F, 0.0F, 0.0F),
+//                    Block.createColumnShape(8.0F, 16.0F, 0.0F, 0.0F),
+//                    Block.createColumnShape(12.0F, 0.0F, 0.0F), RAYCAST_SHAPE),
+//            BooleanBiFunction.ONLY_FIRST));
     public HollowOakLogBlock(Settings settings) {
         super(settings);
     }
 
-    @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return OUTLINE_SHAPE;
-    }
+//    @Override
+//    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+//        return OUTLINE_SHAPE;
+//    }
 
     @Override
     public VoxelShape getRaycastShape(BlockState state, BlockView world, BlockPos pos) {
