bcb42c1fdf26b295f9f72ecde0015cc8c373ca60
cybersage005-netizen <cybersage005@gmail.com>
2026-04-08 21:58:41 +0530

    APRIL=dci-hollow

--- src/client/java/net/greenjab/nekomasfixed/render/block/entity/HollowOakLogBlockEntityRenderer.java
+++ src/client/java/net/greenjab/nekomasfixed/render/block/entity/HollowOakLogBlockEntityRenderer.java
@@ -3,19 +3,31 @@ package net.greenjab.nekomasfixed.render.block.entity;
 import net.greenjab.nekomasfixed.registry.block.entity.HollowOakLogBlockEntity;
 import net.greenjab.nekomasfixed.render.block.entity.state.HollowOakLogBlockEntityRenderState;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
+import net.minecraft.client.render.command.ModelCommandRenderer;
 import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Vec3d;
+import org.jspecify.annotations.Nullable;
 
 public class HollowOakLogBlockEntityRenderer implements BlockEntityRenderer<HollowOakLogBlockEntity, HollowOakLogBlockEntityRenderState>{
+    BlockState state = Blocks.AIR.getDefaultState();
 
     @Override
     public HollowOakLogBlockEntityRenderState createRenderState() {
         return new HollowOakLogBlockEntityRenderState();
+    }
+
+    @Override
+    public void updateRenderState(HollowOakLogBlockEntity blockEntity, HollowOakLogBlockEntityRenderState state, float tickProgress, Vec3d cameraPos, ModelCommandRenderer.@Nullable CrumblingOverlayCommand crumblingOverlay) {
+        this.state = blockEntity.getStoredBlock();
+        BlockEntityRenderState.updateBlockEntityRenderState(blockEntity, state, crumblingOverlay);
     }
 
     @Override
