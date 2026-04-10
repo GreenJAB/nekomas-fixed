de5f4183bbed3f78435771b5a1f03305b913afd0
cybersage005-netizen <cybersage005@gmail.com>
2026-04-10 15:53:09 +0530

    APRIL=dci-hollow

--- src/main/java/net/greenjab/nekomasfixed/registry/block/AbstractHollowLogBlock.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/block/AbstractHollowLogBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.Util;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
@@ -12,7 +13,9 @@ import org.jspecify.annotations.Nullable;
 
 public class AbstractHollowLogBlock extends PillarBlock implements BlockEntityProvider{
     private static final VoxelShape RAYCAST_SHAPE = Block.createCuboidShape(2, 0, 2, 14, 16, 14);
-    public static final VoxelShape OUTLINE_SHAPE = Util.make(() -> VoxelShapes.combineAndSimplify(
+    private static final VoxelShape OUTLINE_X = Block.createCuboidShape(0, 2, 2, 16, 14, 14);
+    private static final VoxelShape OUTLINE_Z = Block.createCuboidShape(2, 2, 0, 14, 14, 16);
+    public static final VoxelShape OUTLINE_Y = Util.make(() -> VoxelShapes.combineAndSimplify(
             VoxelShapes.fullCube(),
             VoxelShapes.union(Block.createColumnShape(16.0F, 8.0F, 0.0F, 0.0F),
                     Block.createColumnShape(8.0F, 16.0F, 0.0F, 0.0F),
@@ -24,7 +27,12 @@ public class AbstractHollowLogBlock extends PillarBlock implements BlockEntityPr
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return OUTLINE_SHAPE;
+        Direction.Axis axis = state.get(AXIS);
+        return switch (axis) {
+            case X -> OUTLINE_X;
+            case Y -> OUTLINE_Y;
+            case Z -> OUTLINE_Z;
+        };
     }
 
     @Override
