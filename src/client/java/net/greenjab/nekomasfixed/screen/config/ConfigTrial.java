3f0bee086fb69e6369c5de10a7f15330f83c634f
cybersage005-netizen <cybersage005@gmail.com>
2026-04-14 12:45:29 +0530

    APRIL=dci-termites

--- src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
+++ src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
@@ -2,8 +2,11 @@ package net.greenjab.nekomasfixed.screen.config;
 
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ConfigTrial {
 
@@ -13,6 +16,13 @@ public class ConfigTrial {
                 .setTitle(Text.literal("Nekomas Fixed Config"));
 
         ConfigCategory emptyCategory = builder.getOrCreateCategory(Text.literal("Main Features"));
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+        AtomicReference<String> currentValue = new AtomicReference<>("option.namefield.optionA");
+        emptyCategory.addEntry(entryBuilder.startStrField(Text.literal("option.namefield.optionB"), currentValue.get())
+                .setDefaultValue("This is the default value")
+                .setTooltip(Text.literal("This option is awesome!"))
+                .setSaveConsumer(currentValue::set)
+                .build());
 
         builder.setSavingRunnable(() -> {
             System.out.println("Config saved! (Still nothing to save yet)");
