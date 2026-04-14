e622ddb882f8731723a81905cfbd6b6fa89ab071
cybersage005-netizen <cybersage005@gmail.com>
2026-04-14 14:08:36 +0530

    APRIL=dci-config

--- src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
+++ src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
@@ -3,6 +3,7 @@ package net.greenjab.nekomasfixed.screen.config;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.block.MapColor;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
@@ -15,10 +16,12 @@ public class ConfigTrial {
                 .setParentScreen(parentScreen)
                 .setTitle(Text.literal("Nekomas Fixed Config"));
 
-        ConfigCategory netherCategory = builder.getOrCreateCategory(Text.literal("Main Features"));
+        ConfigCategory netherCategory = builder.getOrCreateCategory(Text.literal("Nether Features"));
+        ConfigCategory worldCategory = builder.getOrCreateCategory(Text.literal("World Features"));
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-        AtomicReference<String> currentValue = new AtomicReference<>("Name: ");
+        AtomicReference<Boolean> doFoodRotting = new AtomicReference<>(true);
+        AtomicReference<Boolean> enableCopperBuff = new AtomicReference<>(true);
 
         netherCategory.addEntry(
                 entryBuilder.startTextDescription(
@@ -26,14 +29,25 @@ public class ConfigTrial {
                 ).build()
         );
 
-        netherCategory.addEntry(entryBuilder.startStrField(Text.literal("Enter your name"), currentValue.get())
-                .setDefaultValue("Your name")
-                .setTooltip(Text.literal("This option takes name as the user input!"))
-                .setSaveConsumer(currentValue::set)
+        netherCategory.addEntry(entryBuilder.startBooleanToggle(Text.literal("Do Food Rotting"), doFoodRotting.get())
+                .setDefaultValue(true)
+                .setTooltip(Text.literal("All food items except for the golden ones rot in nether over time"))
+                .setSaveConsumer(doFoodRotting::set)
                 .build());
 
+        worldCategory.addEntry(
+                entryBuilder.startTextDescription(
+                        Text.literal("=== World Improvements ===").withColor(MapColor.LIGHT_BLUE.color)
+                ).build()
+        );
 
-        builder.setSavingRunnable(() -> System.out.println("Config saved! :" + currentValue));
+        worldCategory.addEntry(entryBuilder.startBooleanToggle(Text.literal("Enable Copper Buffs"), ModConfigValues.enableCopperBuff)
+                .setDefaultValue(true)
+                .setTooltip(Text.literal("Lightning striking a player with full copper gear would give the player speed "))
+                .setSaveConsumer((val)-> ModConfigValues.enableCopperBuff = val)
+                .build());
+
+        builder.setSavingRunnable(() -> System.out.println("Config saved! :" + enableCopperBuff));
 
         return builder.build();
     }
--- src/main/java/net/greenjab/nekomasfixed/mixin/ServerWorldMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/ServerWorldMixin.java
@@ -1,5 +1,6 @@
 package net.greenjab.nekomasfixed.mixin;
 
+import net.greenjab.nekomasfixed.screen.config.ModConfigValues;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LightningEntity;
@@ -8,7 +9,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
@@ -35,7 +35,7 @@ class ServerWorldMixin {
     @Shadow
     public abstract <T extends ParticleEffect> int spawnParticles(T parameters, double x, double y, double z, int count, double offsetX, double offsetY, double offsetZ, double speed);
 
-    @Inject(method = "tickThunder", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tickThunder", at = @At("HEAD"))
     private void tickThunder(WorldChunk chunk, CallbackInfo ci) {
         ChunkPos chunkPos = chunk.getPos();
         ServerWorld serverWorld = (ServerWorld) (Object)this;
@@ -43,23 +43,24 @@ class ServerWorldMixin {
         Profiler profiler = Profilers.get();
         profiler.push("thunder");
         ServerPlayerEntity player = this.getRandomAlivePlayer();
-        if (bl && serverWorld.isThundering() && serverWorld.random.nextInt(10000) == 0 &&
-               player.getEquippedStack(EquipmentSlot.HEAD).isOf(Items.COPPER_HELMET)&&
-               player.getEquippedStack(EquipmentSlot.CHEST).isOf(Items.COPPER_CHESTPLATE)&&
-               player.getEquippedStack(EquipmentSlot.LEGS).isOf(Items.COPPER_LEGGINGS)&&
-               player.getEquippedStack(EquipmentSlot.FEET).isOf(Items.COPPER_BOOTS)
-        )
-        {
-            BlockPos blockPos = Objects.requireNonNull(player).getBlockPos();
-            if (serverWorld.hasRain(blockPos)) {
-                LightningEntity lightningEntity = (LightningEntity)EntityType.LIGHTNING_BOLT.create(serverWorld, SpawnReason.EVENT);
-                if (lightningEntity != null) {
-                    lightningEntity.refreshPositionAfterTeleport(Vec3d.ofBottomCenter(blockPos));
-                    StatusEffectInstance st = new StatusEffectInstance(StatusEffects.SPEED, 10*20, 4, false, false, false);
-                    StatusEffectInstance stH = new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1*20, 2, false, false, false);
-                    player.addStatusEffect(st);
-                    player.addStatusEffect(stH);
-                    serverWorld.spawnEntity(lightningEntity);
+        if (ModConfigValues.enableCopperBuff && bl && serverWorld.isThundering() && serverWorld.random.nextInt(10000) == 0
+        ) {
+            assert player != null;
+            if ( player.getEquippedStack(EquipmentSlot.HEAD).isOf(Items.COPPER_HELMET) &&
+                    player.getEquippedStack(EquipmentSlot.CHEST).isOf(Items.COPPER_CHESTPLATE) &&
+                    player.getEquippedStack(EquipmentSlot.LEGS).isOf(Items.COPPER_LEGGINGS) &&
+                    player.getEquippedStack(EquipmentSlot.FEET).isOf(Items.COPPER_BOOTS)) {
+                BlockPos blockPos = Objects.requireNonNull(player).getBlockPos();
+                if (serverWorld.hasRain(blockPos)) {
+                    LightningEntity lightningEntity = (LightningEntity) EntityType.LIGHTNING_BOLT.create(serverWorld, SpawnReason.EVENT);
+                    if (lightningEntity != null) {
+                        lightningEntity.refreshPositionAfterTeleport(Vec3d.ofBottomCenter(blockPos));
+                        StatusEffectInstance st = new StatusEffectInstance(StatusEffects.SPEED, 10 * 20, 4, false, false, false);
+                        StatusEffectInstance stH = new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1 * 20, 2, false, false, false);
+                        player.addStatusEffect(st);
+                        player.addStatusEffect(stH);
+                        serverWorld.spawnEntity(lightningEntity);
+                    }
                 }
             }
         }
