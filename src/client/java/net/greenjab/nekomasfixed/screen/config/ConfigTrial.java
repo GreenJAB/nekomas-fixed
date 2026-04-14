119e32ae42fde8c422a3e8df177e3412a42b68a1
cybersage005-netizen <cybersage005@gmail.com>
2026-04-14 13:28:41 +0530

    APRIL=dci-config

--- src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
+++ src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
@@ -3,6 +3,7 @@ package net.greenjab.nekomasfixed.screen.config;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.block.MapColor;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.StringVisitable;
@@ -21,7 +22,8 @@ public class ConfigTrial {
         ConfigCategory emptyCategory = builder.getOrCreateCategory(Text.literal("Main Features"));
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
         emptyCategory.setDescription(()->{
-            Optional<StringVisitable[]> text = Optional.of(new MutableText[]{Text.literal("Nether Improvements")});
+            Optional<StringVisitable[]> text = Optional.of(new MutableText[]{Text.literal("Nether Improvements").withColor(MapColor.RED.color)});
+
             return text;
         });
         AtomicReference<String> currentValue = new AtomicReference<>("Name: ");
