55d1e25e8e8e327c1e753194cdbc0642e4892d58
cybersage005-netizen <cybersage005@gmail.com>
2026-04-14 13:43:10 +0530

    APRIL=dci-config

--- src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
+++ src/client/java/net/greenjab/nekomasfixed/screen/config/ConfigTrial.java
@@ -4,7 +4,6 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -16,18 +15,23 @@ public class ConfigTrial {
                 .setParentScreen(parentScreen)
                 .setTitle(Text.literal("Nekomas Fixed Config"));
 
-        ConfigCategory emptyCategory = builder.getOrCreateCategory(Text.literal("Main Features"));
+        ConfigCategory netherCategory = builder.getOrCreateCategory(Text.literal("Main Features"));
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-        emptyCategory.setDescription(
-                new MutableText[]{Text.literal("Nether Improvements").withColor(0xBA2720)}
-        );
 
         AtomicReference<String> currentValue = new AtomicReference<>("Name: ");
-        emptyCategory.addEntry(entryBuilder.startStrField(Text.literal("Enter your name"), currentValue.get())
+
+        netherCategory.addEntry(
+                entryBuilder.startTextDescription(
+                        Text.literal("=== Nether Improvements ===").withColor(0xBA2720)
+                ).build()
+        );
+
+        netherCategory.addEntry(entryBuilder.startStrField(Text.literal("Enter your name"), currentValue.get())
                 .setDefaultValue("Your name")
                 .setTooltip(Text.literal("This option takes name as the user input!"))
                 .setSaveConsumer(currentValue::set)
                 .build());
+
 
         builder.setSavingRunnable(() -> System.out.println("Config saved! :" + currentValue));
 
