93357c7a612e7416b949762dd2245a40310e2b13
cybersage005-netizen <cybersage005@gmail.com>
2026-04-17 17:11:38 +0530

    APRIL=DCI-config

--- src/main/java/net/greenjab/nekomasfixed/mixin/AbstractBlockMixin.java
+++ src/main/java/net/greenjab/nekomasfixed/mixin/AbstractBlockMixin.java
@@ -29,7 +29,7 @@ public class AbstractBlockMixin {
                 Direction direction = hit.getSide();
                 BlockPos placePos = pos.offset(direction);
 
-                BlockState newState = BlockRegistry.GOAT_HORN.getDefaultState();
+                BlockState newState = BlockRegistry.GOAT_HORN.getDefaultState().with(HorizontalFacingBlock.FACING, direction.getOpposite());
                 if (newState.contains(Properties.HORIZONTAL_FACING)) {
                     newState = newState.with(Properties.HORIZONTAL_FACING, player.getHorizontalFacing().getOpposite());
                 }
