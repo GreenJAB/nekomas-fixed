ed305746df0ef8f9ae5a6676407544408e79c509
joshua <joshua@baikie.org>
2026-08-11 11:30:47 +1000

    moobloom snouts

--- src/client/java/net/greenjab/nekomasfixed/render/entity/model/MoobloomEntityModel.java
+++ src/client/java/net/greenjab/nekomasfixed/render/entity/model/MoobloomEntityModel.java
@@ -31,9 +31,19 @@ public class MoobloomEntityModel extends QuadrupedEntityModel<MoobloomEntityRend
 
         modelPartData.addChild("bone", ModelPartBuilder.create(), ModelTransform.origin(0.0F, 24.0F, 2.0F));
 
-        ModelPartData head = modelPartData.addChild("head", ModelPartBuilder.create().uv(22, 0).cuboid(-5.0F, -5.0F, -4.0F, 1.0F, 3.0F, 1.0F, new Dilation(0.0F))
-                .uv(0, 0).cuboid(-4.0F, -4.0F, -6.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.0F))
-                .uv(22, 0).cuboid(4.0F, -5.0F, -4.0F, 1.0F, 3.0F, 1.0F, new Dilation(0.0F)), ModelTransform.origin(0.0F, 4.0F, -8.0F) );
+        ModelPartData head = modelPartData.addChild(
+                "head",
+                ModelPartBuilder.create()
+                        .uv(0, 0)
+                        .cuboid(-4.0F, -4.0F, -6.0F, 8.0F, 8.0F, 6.0F)
+                        .uv(1, 33)
+                        .cuboid(-3.0F, 1.0F, -7.0F, 6.0F, 3.0F, 1.0F)
+                        .uv(22, 0)
+                        .cuboid("right_horn", -5.0F, -5.0F, -5.0F, 1.0F, 3.0F, 1.0F)
+                        .uv(22, 0)
+                        .cuboid("left_horn", 4.0F, -5.0F, -5.0F, 1.0F, 3.0F, 1.0F),
+                ModelTransform.origin(0.0F, 4.0F, -8.0F)
+        );
         ModelPartData body = modelPartData.addChild("body",ModelPartBuilder.create(),ModelTransform.origin(0.0F, 5.0F, 2.0F));
         modelPartData.addChild("right_hind_leg", ModelPartBuilder.create().uv(0, 16)
                         .cuboid(-2.0F, 0.0F, -2.0F, 4, 12, 4),ModelTransform.origin(-4.0F, 12.0F, 7.0F));
@@ -47,14 +57,14 @@ public class MoobloomEntityModel extends QuadrupedEntityModel<MoobloomEntityRend
         ModelPartData rotation = body.addChild("rotation", ModelPartBuilder.create().uv(18, 4).cuboid(-6.0F, -10.0F, -7.0F, 12.0F, 18.0F, 10.0F, new Dilation(0.0F))
                 .uv(52, 0).cuboid(-2.0F, 2.0F, -8.0F, 4.0F, 6.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 0.0F, 0.0F, 1.5708F, 0.0F, 0.0F));
 
-        ModelPartData flower1 = rotation.addChild("flower1", ModelPartBuilder.create().uv(0, 32).cuboid(-7.975F, -16.0F, 1.2F, 16.0F, 16.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-2.025F, -2.0F, 2.8F, -1.5708F, 0.0F, 0.0F));
-        flower1.addChild("cube_r1", ModelPartBuilder.create().uv(0, 16).cuboid(0.0F, -8.0F, -8.0F, 0.0F, 16.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(0.025F, -8.0F, 1.2F, 0.0F, 3.1416F, 0.0F));
+        ModelPartData flower1 = rotation.addChild("flower1", ModelPartBuilder.create().uv(0, 42).cuboid(-7.975F, -16.0F, 1.2F, 16.0F, 16.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-2.025F, -2.0F, 2.8F, -1.5708F, 0.0F, 0.0F));
+        flower1.addChild("cube_r1", ModelPartBuilder.create().uv(0, 26).cuboid(0.0F, -8.0F, -8.0F, 0.0F, 16.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(0.025F, -8.0F, 1.2F, 0.0F, 3.1416F, 0.0F));
 
-        ModelPartData flower2 = rotation.addChild("flower2", ModelPartBuilder.create().uv(0, 32).cuboid(-5.95F, -16.0F, 0.0F, 16.0F, 16.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(2.95F, 5.0F, 3.0F, -1.5708F, 0.0F, 0.7854F));
-        flower2.addChild("cube_r2", ModelPartBuilder.create().uv(0, 16).cuboid(0.0F, -8.0F, -8.0F, 0.0F, 16.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(2.05F, -8.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+        ModelPartData flower2 = rotation.addChild("flower2", ModelPartBuilder.create().uv(0, 42).cuboid(-5.95F, -16.0F, 0.0F, 16.0F, 16.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(2.95F, 5.0F, 3.0F, -1.5708F, 0.0F, 0.7854F));
+        flower2.addChild("cube_r2", ModelPartBuilder.create().uv(0, 26).cuboid(0.0F, -8.0F, -8.0F, 0.0F, 16.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(2.05F, -8.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
         
-        ModelPartData flower3 = head.addChild("flower3", ModelPartBuilder.create().uv(0, 32).cuboid(-8.0F, -16.0F, 0.0F, 16.0F, 16.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -4.0F, -3.2F, 0.0F, -0.576F, 0.0F));
-        flower3.addChild("cube_r3", ModelPartBuilder.create().uv(0, 16).cuboid(0.0F, -8.0F, -8.0F, 0.0F, 16.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -8.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+        ModelPartData flower3 = head.addChild("flower3", ModelPartBuilder.create().uv(0, 42).cuboid(-8.0F, -16.0F, 0.0F, 16.0F, 16.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -4.0F, -3.2F, 0.0F, -0.576F, 0.0F));
+        flower3.addChild("cube_r3", ModelPartBuilder.create().uv(0, 26).cuboid(0.0F, -8.0F, -8.0F, 0.0F, 16.0F, 16.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, -8.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
         return modelData;
     }
